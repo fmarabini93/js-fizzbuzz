520069f49e2958796810c1fd790d7275b061e31b
fmarabini93 <fmarabini93>
2021-04-29 15:23:15 +0200

    ul

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
+var num = [];
+
 for (i=1; i<101; i++) {
       if (i % 3 == 0 && i % 5 == 0) {
+            num.push("FizzBuzz");
             console.log("FizzBuzz");
+
       } else if (i % 3 == 0) {
+            num.push("Fizz");
             console.log("Fizz");
       } else if (i % 5 == 0) {
+            num.push("Buzz");
             console.log("Buzz");
       } else {
+            num.push(i);
             console.log(i);
       }
+}
+
+for (i=0; i<num.length; i++) {
+      document.getElementById("numbers").innerHTML += "<li>" + num[i] + "</li>";
 }
